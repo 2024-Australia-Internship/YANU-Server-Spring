4816b1e4f0ea32ffc70485b91e15ce52e124519c
seonyo <jsy6535@naver.com>
2024-04-07 07:57:18 +0900

    ✨ RegisterRequestDto 추가

--- src/main/java/com/bbogle/yanu/dto/user/RegisterRequestDto.java
+++ src/main/java/com/bbogle/yanu/dto/user/RegisterRequestDto.java
@@ -1,5 +1,6 @@
 package com.bbogle.yanu.dto.user;
 
+import com.bbogle.yanu.entity.UserEntity;
 import lombok.Getter;
 
 @Getter
@@ -7,4 +8,12 @@ public class RegisterRequestDto {
     private String email;
     private String password;
     private String phonenumber;
+
+    public UserEntity toEntity(String password){
+        return UserEntity.builder()
+                .email(email)
+                .password(password)
+                .phonenumber(phonenumber)
+                .build();
+    }
 }
