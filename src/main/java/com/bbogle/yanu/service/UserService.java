3a31e9d2a272c023fd2a00d482643cd934f81ec8
seonyo <jsy6535@naver.com>
2024-04-07 07:18:56 +0900

    ✨ UserService 생성

---
+++ src/main/java/com/bbogle/yanu/service/UserService.java
@@ -0,0 +1,13 @@
+package com.bbogle.yanu.service;
+
+import com.bbogle.yanu.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+}
