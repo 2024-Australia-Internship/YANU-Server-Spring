6ceb8ec9f11b3ee780c0bc6a690db694abe34e69
seonyo <jsy6535@naver.com>
2024-04-07 07:57:26 +0900

    ✨ 회원가입 서비스 추가

--- src/main/java/com/bbogle/yanu/service/UserService.java
+++ src/main/java/com/bbogle/yanu/service/UserService.java
@@ -1,13 +1,33 @@
 package com.bbogle.yanu.service;
 
+import com.bbogle.yanu.dto.user.RegisterRequestDto;
+import com.bbogle.yanu.exception.EmailDuplicateException;
+import com.bbogle.yanu.exception.error.ErrorCode;
 import com.bbogle.yanu.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void registerUser(RegisterRequestDto request){
+        String email = request.getEmail();
+        String password = passwordEncoder.encode(request.getPassword());
+
+        duplicateEmail(email);
+
+        userRepository.save(request.toEntity(password));
+    }
+
+    void duplicateEmail(String email){
+        if(userRepository.existsByEmail(email)){
+            throw new EmailDuplicateException("email duplicated", ErrorCode.EMAIL_DUPLICATION);
+        }
+    }
 
 }
