d044b65c46a9c977ef803400465c88d9ecc6137d
seonyo <jsy6535@naver.com>
2024-04-07 07:18:42 +0900

    ✨ UserController 생성

---
+++ src/main/java/com/bbogle/yanu/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.bbogle.yanu.controller;
+
+import com.bbogle.yanu.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+}
