64d0d8f9157199e7b6af09192a0424e764c21d4a
seonyo <jsy6535@naver.com>
2024-04-07 07:07:59 +0900

    ✨  User 엔티티완성

---
+++ src/main/java/com/bbogle/yanu/entity/UserEntity.java
@@ -0,0 +1,41 @@
+package com.bbogle.yanu.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity (name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @JsonIgnore
+    @Column
+    private String password;
+
+    @Column(unique = true)
+    private String phonenumber;
+
+    @Column
+    private String profile_image;
+
+    @Column
+    private String nickname;
+
+    @Column(columnDefinition = "TEXT")
+    private String introduction;
+
+    @Column
+    private Byte ugly_percent;
+
+    @Column
+    private Boolean is_farmer;
+}
