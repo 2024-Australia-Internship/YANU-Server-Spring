7f7a92c81a56f51ab86a0c4ca87bcfde427e5474
seonyo <jsy6535@naver.com>
2024-04-07 07:57:33 +0900

    ✨ 회원가입 컨트롤러 추가

--- src/main/java/com/bbogle/yanu/controller/UserController.java
+++ src/main/java/com/bbogle/yanu/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.bbogle.yanu.controller;
 
+import com.bbogle.yanu.dto.user.RegisterRequestDto;
+import com.bbogle.yanu.entity.UserEntity;
 import com.bbogle.yanu.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -14,5 +17,11 @@ public class UserController {
     @Autowired
     public UserController(UserService userService){
         this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> registerUser(@RequestBody RegisterRequestDto request) {
+        userService.registerUser(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body("회원가입에 성공했습니다");
     }
 }
