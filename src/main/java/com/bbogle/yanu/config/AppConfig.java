8cab1988753e53a436948733c22935d30fdd6519
seonyo <jsy6535@naver.com>
2024-04-07 07:18:17 +0900

    ✨ AppConfig 생성

---
+++ src/main/java/com/bbogle/yanu/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.bbogle.yanu.config;
+
+import com.bbogle.yanu.repository.UserRepository;
+import com.bbogle.yanu.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@AllArgsConstructor
+@Configuration
+public class AppConfig {
+}
