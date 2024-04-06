fcc4e8f0145a26c686a1586ca45406d64505e50c
seonyo <jsy6535@naver.com>
2024-04-07 07:55:57 +0900

    ✨ EmailDuplicate 에러 핸들링 추가

--- src/main/java/com/bbogle/yanu/exception/error/GlobalExceptionHandler.java
+++ src/main/java/com/bbogle/yanu/exception/error/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.bbogle.yanu.exception.error;
 
+import com.bbogle.yanu.exception.EmailDuplicateException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,5 +15,12 @@ public class GlobalExceptionHandler {
         log.error("handleException",ex);
         ErrorResponse response = new ErrorResponse(ErrorCode.INTER_SERVER_ERROR);
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(EmailDuplicateException.class)
+    public ResponseEntity<ErrorResponse> handleEmailDuplicateException(EmailDuplicateException ex){
+        log.error("handleEmailDuplicateException",ex);
+        ErrorResponse response = new ErrorResponse(ex.getErrorCode());
+        return new ResponseEntity<>(response, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
     }
 }
