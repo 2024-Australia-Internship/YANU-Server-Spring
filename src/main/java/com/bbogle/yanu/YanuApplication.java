800cc008f9db0be0739b2c7fec6e0df41603d143
seonyo <jsy6535@naver.com>
2024-04-07 06:44:59 +0900

    :tada: 프로젝트 초기 환경 셋팅

---
+++ src/main/java/com/bbogle/yanu/YanuApplication.java
@@ -0,0 +1,13 @@
+package com.bbogle.yanu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YanuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbogle/yanu/YanuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbogle.yanu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
