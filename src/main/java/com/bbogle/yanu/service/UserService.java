91e9d06e445ebe5287cf2a5acf4af81c24cfb642
seonyo <jsy6535@naver.com>
2024-04-07 16:04:26 +0900

    ✨ Login Service 추가

--- src/main/java/com/bbogle/yanu/service/UserService.java
+++ src/main/java/com/bbogle/yanu/service/UserService.java
@@ -1,10 +1,12 @@
 package com.bbogle.yanu.service;
 
+import com.bbogle.yanu.dto.user.LoginRequestDto;
 import com.bbogle.yanu.dto.user.RegisterRequestDto;
+import com.bbogle.yanu.entity.UserEntity;
 import com.bbogle.yanu.exception.EmailDuplicateException;
+import com.bbogle.yanu.exception.PasswordNotFoundException;
 import com.bbogle.yanu.exception.error.ErrorCode;
 import com.bbogle.yanu.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,11 +25,30 @@ public class UserService {
 
         userRepository.save(request.toEntity(password));
     }
-
     void duplicateEmail(String email){
         if(userRepository.existsByEmail(email)){
             throw new EmailDuplicateException("email duplicated", ErrorCode.EMAIL_DUPLICATION);
         }
     }
+    public Long loginUser(LoginRequestDto request){
+        String email = request.getEmail();
+        String password = request.getPassword();
 
+        checkEmail(email);
+
+        UserEntity userEntity = userRepository.findByEmail(email);
+        checkPassword(password, userEntity.getPassword());
+
+        return userEntity.getId();
+    }
+    private void checkEmail(String email){
+        if(!userRepository.existsByEmail(email)){
+            throw new EmailDuplicateException("email not found", ErrorCode.EMAIL_NOTFOUND);
+        }
+    }
+    private void checkPassword(String password, String hashedPassword){
+        if(!passwordEncoder.matches(password, hashedPassword)){
+            throw new PasswordNotFoundException("password not found", ErrorCode.PASSWORD_NOTFOUND);
+        }
+    }
 }
