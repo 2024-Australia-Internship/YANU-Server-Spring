914db51d652620a01dd5cd651ca015cd00e40163
seonyo <jsy6535@naver.com>
2024-04-07 18:06:35 +0900

    ✨ UserIdResponseDto 추가

---
+++ src/main/java/com/bbogle/yanu/dto/user/UserIdResponseDto.java
@@ -0,0 +1,27 @@
+package com.bbogle.yanu.dto.user;
+
+import com.bbogle.yanu.entity.UserEntity;
+import lombok.Getter;
+
+import java.util.Optional;
+
+@Getter
+public class UserIdResponseDto {
+    private String email;
+    private String phonenumber;
+    private String profile_image;
+    private String nickname;
+    private String introduction;
+    private Byte ugly_percent;
+    private Boolean is_farmer;
+
+    public UserIdResponseDto(UserEntity userEntity) {
+        this.email = userEntity.getEmail();
+        this.phonenumber = userEntity.getPhonenumber();
+        this.profile_image = userEntity.getProfile_image();
+        this.nickname = userEntity.getNickname();
+        this.introduction = userEntity.getIntroduction();
+        this.ugly_percent = userEntity.getUgly_percent();
+        this.is_farmer = userEntity.getIs_farmer();
+    }
+}
