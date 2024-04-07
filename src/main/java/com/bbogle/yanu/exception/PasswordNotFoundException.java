e6f61a37ee03d63bfa24c47f06773bd8cab0ce1e
seonyo <jsy6535@naver.com>
2024-04-07 15:53:16 +0900

    :bug: PasswordNotFoundException @Getter 어노테이션 추가

--- src/main/java/com/bbogle/yanu/exception/PasswordNotFoundException.java
+++ src/main/java/com/bbogle/yanu/exception/PasswordNotFoundException.java
@@ -1,7 +1,9 @@
 package com.bbogle.yanu.exception;
 
 import com.bbogle.yanu.exception.error.ErrorCode;
+import lombok.Getter;
 
+@Getter
 public class PasswordNotFoundException extends RuntimeException{
     private ErrorCode errorCode;
 
