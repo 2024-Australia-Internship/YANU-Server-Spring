baec82f5cfe5a60e00f5b7401538839ee9073d73
seonyo <jsy6535@naver.com>
2024-04-09 02:59:31 +0900

    ✨ 농장 등록 서비스 구현

--- src/main/java/com/bbogle/yanu/service/FarmService.java
+++ src/main/java/com/bbogle/yanu/service/FarmService.java
@@ -1,4 +1,16 @@
 package com.bbogle.yanu.service;
 
+import com.bbogle.yanu.dto.farm.RegisterFarmRequestDto;
+import com.bbogle.yanu.repository.FarmRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class FarmService {
+    private final FarmRepository farmRepository;
+
+    public void registerFarm(RegisterFarmRequestDto request){
+        farmRepository.save(request.toEntity());
+    }
 }
