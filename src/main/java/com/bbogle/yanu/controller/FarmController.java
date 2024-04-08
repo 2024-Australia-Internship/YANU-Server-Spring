73a60e3bf0826b59a01fef7c641c5f8fab489149
seonyo <jsy6535@naver.com>
2024-04-09 01:50:42 +0900

    ✨ FarmController 생성

---
+++ src/main/java/com/bbogle/yanu/controller/FarmController.java
@@ -0,0 +1,9 @@
+package com.bbogle.yanu.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/farms")
+public class FarmController {
+}
