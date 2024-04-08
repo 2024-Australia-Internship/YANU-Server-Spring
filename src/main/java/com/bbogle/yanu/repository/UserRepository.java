29629f0315950d742a419101b0b279c6dd30c31e
seonyo <jsy6535@naver.com>
2024-04-09 01:30:43 +0900

    ✨ existsByPhonenumber 메소드 추가

--- src/main/java/com/bbogle/yanu/repository/UserRepository.java
+++ src/main/java/com/bbogle/yanu/repository/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByEmail(String email);
-
+    boolean existsByPhonenumber(String phonenumber);
     UserEntity findByEmail(String email);
 }
