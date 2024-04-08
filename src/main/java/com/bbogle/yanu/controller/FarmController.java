0b86a59402557208e5c38275e42b5d73e5802858
seonyo <jsy6535@naver.com>
2024-04-09 03:01:19 +0900

    ✨ 농장등록 컨트롤러 구현

--- src/main/java/com/bbogle/yanu/controller/FarmController.java
+++ src/main/java/com/bbogle/yanu/controller/FarmController.java
@@ -1,9 +1,28 @@
 package com.bbogle.yanu.controller;
 
+import com.bbogle.yanu.dto.farm.RegisterFarmRequestDto;
+import com.bbogle.yanu.service.FarmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/farms")
 public class FarmController {
+    private final FarmService farmService;
+
+    @Autowired
+    public FarmController(FarmService farmService){
+        this.farmService = farmService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> registerFarm(@RequestBody RegisterFarmRequestDto request){
+        farmService.registerFarm(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body("농장 등록 성공했습니다.");
+    }
 }
