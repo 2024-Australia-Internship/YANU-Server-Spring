c9939f50eebc474d0024bf1f716d41688b38bdf3
seonyo <jsy6535@naver.com>
2024-04-10 00:07:43 +0900

    ✨ findByUserId 메소드 추가

--- src/main/java/com/bbogle/yanu/repository/FarmRepository.java
+++ src/main/java/com/bbogle/yanu/repository/FarmRepository.java
@@ -4,4 +4,5 @@ import com.bbogle.yanu.entity.FarmEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FarmRepository extends JpaRepository<FarmEntity, Long> {
+    FarmEntity findByUserId(Long user_id);
 }
