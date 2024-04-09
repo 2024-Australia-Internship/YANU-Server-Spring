607dc6ffd0265ec948a9d20518a20055947c0848
seonyo <jsy6535@naver.com>
2024-04-10 00:08:19 +0900

    ✨ user의 농장 정보 조회 컨트롤러 구현

--- src/main/java/com/bbogle/yanu/controller/FarmController.java
+++ src/main/java/com/bbogle/yanu/controller/FarmController.java
@@ -1,14 +1,13 @@
 package com.bbogle.yanu.controller;
 
+import com.bbogle.yanu.dto.farm.FarmUserResponseDto;
 import com.bbogle.yanu.dto.farm.RegisterFarmRequestDto;
+import com.bbogle.yanu.entity.FarmEntity;
 import com.bbogle.yanu.service.FarmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/farms")
@@ -24,5 +23,11 @@ public class FarmController {
     public ResponseEntity<String> registerFarm(@RequestBody RegisterFarmRequestDto request){
         farmService.registerFarm(request);
         return ResponseEntity.status(HttpStatus.CREATED).body("농장 등록 성공했습니다.");
+    }
+
+    @GetMapping("/{user_id}")
+    public ResponseEntity<FarmUserResponseDto> FarmFindByUser (@PathVariable("user_id") Long id){
+        FarmEntity farmEntity = farmService.framFindByUser(id);
+        return ResponseEntity.ok().body(new FarmUserResponseDto(farmEntity));
     }
 }
