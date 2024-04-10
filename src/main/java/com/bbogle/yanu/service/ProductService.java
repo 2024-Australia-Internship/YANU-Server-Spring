34123bc2376b447b485b27ed6e3e6519d456917f
seonyo <jsy6535@naver.com>
2024-04-11 00:28:05 +0900

    ✨ 상품 등록하는 서비스 구현

--- src/main/java/com/bbogle/yanu/service/ProductService.java
+++ src/main/java/com/bbogle/yanu/service/ProductService.java
@@ -1,4 +1,16 @@
 package com.bbogle.yanu.service;
 
+import com.bbogle.yanu.dto.product.RegisterProductRequestDto;
+import com.bbogle.yanu.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class ProductService {
+    private final ProductRepository  productRepository;
+
+    public void registerProduct(RegisterProductRequestDto request){
+        productRepository.save(request.toEntity());
+    }
 }
