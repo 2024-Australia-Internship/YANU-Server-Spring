526f66bbf093c9a6c5b9c0256b64e6f8690a0c39
seonyo <jsy6535@naver.com>
2024-04-11 00:44:26 +0900

    ✨ 모든 상품을 조회하는 서비스 구현

--- src/main/java/com/bbogle/yanu/service/ProductService.java
+++ src/main/java/com/bbogle/yanu/service/ProductService.java
@@ -1,9 +1,12 @@
 package com.bbogle.yanu.service;
 
 import com.bbogle.yanu.dto.product.RegisterProductRequestDto;
+import com.bbogle.yanu.entity.ProductEntity;
 import com.bbogle.yanu.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -12,5 +15,9 @@ public class ProductService {
 
     public void registerProduct(RegisterProductRequestDto request){
         productRepository.save(request.toEntity());
+    }
+
+    public List<ProductEntity> findAllProduct(){
+        return productRepository.findAll();
     }
 }
