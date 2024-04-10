18f18d48fb432345fa0305a9cec39be6a2ebe7b0
seonyo <jsy6535@naver.com>
2024-04-11 00:28:16 +0900

    ✨ 상품 등록하는 컨트롤러 구현

--- src/main/java/com/bbogle/yanu/controller/ProductController.java
+++ src/main/java/com/bbogle/yanu/controller/ProductController.java
@@ -1,4 +1,25 @@
 package com.bbogle.yanu.controller;
 
+import com.bbogle.yanu.dto.product.RegisterProductRequestDto;
+import com.bbogle.yanu.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/products")
 public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<String> registerProduct(@RequestBody RegisterProductRequestDto request){
+        productService.registerProduct(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body("상품 등록 성공했습니다");
+    }
+
 }
