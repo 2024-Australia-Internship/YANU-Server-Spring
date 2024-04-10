b3eca56870f084751c0a5e7e5ed70c2d62e51372
seonyo <jsy6535@naver.com>
2024-04-11 00:44:34 +0900

    ✨ 모든 상품을 조회하는 컨트롤러 구현

--- src/main/java/com/bbogle/yanu/controller/ProductController.java
+++ src/main/java/com/bbogle/yanu/controller/ProductController.java
@@ -1,14 +1,16 @@
 package com.bbogle.yanu.controller;
 
+import com.bbogle.yanu.dto.product.ProductResponseDto;
 import com.bbogle.yanu.dto.product.RegisterProductRequestDto;
+import com.bbogle.yanu.entity.ProductEntity;
 import com.bbogle.yanu.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +22,14 @@ public class ProductController {
     public ResponseEntity<String> registerProduct(@RequestBody RegisterProductRequestDto request){
         productService.registerProduct(request);
         return ResponseEntity.status(HttpStatus.CREATED).body("상품 등록 성공했습니다");
+    }
+
+    @GetMapping
+    public List<ProductResponseDto> findAllProduct(){
+        List<ProductEntity> prodcts = productService.findAllProduct();
+        return prodcts.stream()
+                .map(ProductResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }
