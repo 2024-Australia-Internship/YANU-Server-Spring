e38da4fde87a84ff1ce37868f1e8d4c78a82b2c3
seonyo <jsy6535@naver.com>
2024-04-11 08:16:22 +0900

    ✨ 상품 아이디로 상품 정보 조회하는 서비스 구현

--- src/main/java/com/bbogle/yanu/service/ProductService.java
+++ src/main/java/com/bbogle/yanu/service/ProductService.java
@@ -3,11 +3,15 @@ package com.bbogle.yanu.service;
 import com.bbogle.yanu.controller.ProductController;
 import com.bbogle.yanu.dto.product.RegisterProductRequestDto;
 import com.bbogle.yanu.entity.ProductEntity;
+import com.bbogle.yanu.exception.ProductNotFoundException;
+import com.bbogle.yanu.exception.error.ErrorCode;
 import com.bbogle.yanu.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -24,5 +28,10 @@ public class ProductService {
 
     public List<ProductEntity> findAllFarm(Long id){
         return productRepository.findAllByFarmId(id);
+    }
+
+    public ProductEntity findByProduct(Long id){
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException("product not found", ErrorCode.PRODUCT_NOTFOUND));
     }
 }
