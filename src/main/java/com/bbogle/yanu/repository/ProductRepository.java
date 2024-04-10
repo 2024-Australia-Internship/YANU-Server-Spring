03e25cc7eaeb7a23346b8dd2d8a881e96ca8e035
seonyo <jsy6535@naver.com>
2024-04-11 00:44:07 +0900

    ✨ ProductRepository JPARepository 상속

--- src/main/java/com/bbogle/yanu/repository/ProductRepository.java
+++ src/main/java/com/bbogle/yanu/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.bbogle.yanu.repository;
 
-public interface ProductRepository {
+import com.bbogle.yanu.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 }
