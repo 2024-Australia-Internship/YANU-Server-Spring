736c99467270704530400737193c54008a881073
seonyo <jsy6535@naver.com>
2024-04-12 01:46:20 +0900

    ✨ 하트 중복 여부 예외처리

--- src/main/java/com/bbogle/yanu/service/FavoriteService.java
+++ src/main/java/com/bbogle/yanu/service/FavoriteService.java
@@ -1,8 +1,11 @@
 package com.bbogle.yanu.service;
 
 import com.bbogle.yanu.dto.favorite.RegisterHeartDto;
+import com.bbogle.yanu.exception.HeartDuplicateException;
+import com.bbogle.yanu.exception.error.ErrorCode;
 import com.bbogle.yanu.repository.FavoriteRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -11,6 +14,14 @@ public class FavoriteService {
     private final FavoriteRepository favoriteRepository;
 
     public void registerHeart(RegisterHeartDto request){
+        Long userId = request.getUserId().getId();
+        Long productId = request.getProductId().getId();
+        String type = request.getType();
+        boolean exists = favoriteRepository.existsByUserIdAndProductIdAndType(userId, productId, type);
+
+        if(exists){
+            throw new HeartDuplicateException("heart duplicated", ErrorCode.HEART_DUPLICATION);
+        }
         favoriteRepository.save(request.toEntity());
     }
 }
