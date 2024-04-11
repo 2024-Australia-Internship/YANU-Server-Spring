bd2259d17719886f04d4292cc5565e4b84807ce7
seonyo <jsy6535@naver.com>
2024-04-12 02:08:31 +0900

    ✨ deleteByUserIdAndProductIdAndType 메소드 추가

--- src/main/java/com/bbogle/yanu/repository/FavoriteRepository.java
+++ src/main/java/com/bbogle/yanu/repository/FavoriteRepository.java
@@ -1,10 +1,9 @@
 package com.bbogle.yanu.repository;
 
 import com.bbogle.yanu.entity.FavoriteEntity;
-import com.bbogle.yanu.entity.ProductEntity;
-import com.bbogle.yanu.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FavoriteRepository extends JpaRepository<FavoriteEntity, Long> {
     boolean existsByUserIdAndProductIdAndType(Long userId, Long productId, String type);
+    void deleteByUserIdAndProductIdAndType(Long userId, Long productId, String type);
 }
