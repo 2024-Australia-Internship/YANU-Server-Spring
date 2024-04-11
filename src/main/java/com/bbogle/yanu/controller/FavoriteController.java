f067dbd7c32088dc7b10db68d67e0e7e8c39da8d
seonyo <jsy6535@naver.com>
2024-04-12 02:08:44 +0900

    ✨ 하트 등록하는 서비스 구현

--- src/main/java/com/bbogle/yanu/controller/FavoriteController.java
+++ src/main/java/com/bbogle/yanu/controller/FavoriteController.java
@@ -1,5 +1,6 @@
 package com.bbogle.yanu.controller;
 
+import com.bbogle.yanu.dto.favorite.DeleteHeartRequestDto;
 import com.bbogle.yanu.dto.favorite.RegisterHeartRequestDto;
 import com.bbogle.yanu.service.FavoriteService;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,12 @@ public class FavoriteController {
     public ResponseEntity<String> registerHeart(@RequestBody RegisterHeartRequestDto request){
         favoriteService.registerHeart(request);
         return ResponseEntity.status(HttpStatus.CREATED).body("하트 등록에 성공했습니다.");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteHeart(@RequestBody DeleteHeartRequestDto request){
+        favoriteService.deleteHeart(request);
+        return ResponseEntity.ok().body("하트 삭제 성공했습니다.");
     }
 
 }
