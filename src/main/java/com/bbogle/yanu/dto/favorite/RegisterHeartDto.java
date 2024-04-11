0b41e539f64c402efb623961b2bb5024218c3435
seonyo <jsy6535@naver.com>
2024-04-12 01:36:24 +0900

    ✨ RegisterHeartDto 생성

---
+++ src/main/java/com/bbogle/yanu/dto/favorite/RegisterHeartDto.java
@@ -0,0 +1,21 @@
+package com.bbogle.yanu.dto.favorite;
+
+import com.bbogle.yanu.entity.FavoriteEntity;
+import com.bbogle.yanu.entity.ProductEntity;
+import com.bbogle.yanu.entity.UserEntity;
+import lombok.Getter;
+
+@Getter
+public class RegisterHeartDto {
+    private UserEntity userId;
+    private ProductEntity productId;
+    private String type;
+
+    public FavoriteEntity toEntity(){
+        return FavoriteEntity.builder()
+                .user(userId)
+                .product(productId)
+                .type(type)
+                .build();
+    }
+}
