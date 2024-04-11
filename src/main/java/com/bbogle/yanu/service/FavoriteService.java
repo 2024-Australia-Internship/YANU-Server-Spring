9d4f6a6e273776f4262c28d110eda68a210f656a
seonyo <jsy6535@naver.com>
2024-04-12 02:08:40 +0900

    ✨ 하트 삭제하는 서비스 구현

--- src/main/java/com/bbogle/yanu/service/FavoriteService.java
+++ src/main/java/com/bbogle/yanu/service/FavoriteService.java
@@ -1,11 +1,13 @@
 package com.bbogle.yanu.service;
 
+import com.bbogle.yanu.dto.favorite.DeleteHeartRequestDto;
 import com.bbogle.yanu.dto.favorite.RegisterHeartRequestDto;
 import com.bbogle.yanu.exception.HeartDuplicateException;
 import com.bbogle.yanu.exception.error.ErrorCode;
 import com.bbogle.yanu.repository.FavoriteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -22,5 +24,19 @@ public class FavoriteService {
             throw new HeartDuplicateException("heart duplicated", ErrorCode.HEART_DUPLICATION);
         }
         favoriteRepository.save(request.toEntity());
+    }
+
+    @Transactional
+    public void deleteHeart(DeleteHeartRequestDto request){
+        Long userId = request.getUserId().getId();
+        Long productId = request.getProductId().getId();
+        String type = request.getType();
+        boolean exists = favoriteRepository.existsByUserIdAndProductIdAndType(userId, productId, type);
+
+        if(!exists){
+            throw new HeartDuplicateException("heart notfound", ErrorCode.HEART_NOTFOUND);
+        }
+
+        favoriteRepository.deleteByUserIdAndProductIdAndType(userId, productId, type);
     }
 }
