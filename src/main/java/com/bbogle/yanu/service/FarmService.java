6e24323de3a920b4cbec2e03adf94edbe326f52a
seonyo <jsy6535@naver.com>
2024-04-12 16:38:15 +0900

    ✨ 농장등록 서비스 세션 기능 추가

--- src/main/java/com/bbogle/yanu/service/FarmService.java
+++ src/main/java/com/bbogle/yanu/service/FarmService.java
@@ -1,11 +1,14 @@
 package com.bbogle.yanu.service;
 
-import com.bbogle.yanu.dto.farm.FarmUserResponseDto;
 import com.bbogle.yanu.dto.farm.RegisterFarmRequestDto;
 import com.bbogle.yanu.entity.FarmEntity;
-import com.bbogle.yanu.exception.UserNotFoundException;
+import com.bbogle.yanu.entity.UserEntity;
+import com.bbogle.yanu.exception.SessionNotFoundException;
 import com.bbogle.yanu.exception.error.ErrorCode;
 import com.bbogle.yanu.repository.FarmRepository;
+import com.bbogle.yanu.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +16,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class FarmService {
     private final FarmRepository farmRepository;
+    private final UserRepository userRepository;
 
-    public void registerFarm(RegisterFarmRequestDto request){
+    public void registerFarm(RegisterFarmRequestDto request, HttpServletRequest httpRequest){
+        HttpSession session = httpRequest.getSession(false);
+
+        if(session == null){
+            throw new SessionNotFoundException("session not found", ErrorCode.SESSION_NOTFOUND);
+        }
+
+        Long id = (Long) session.getAttribute("userId");
+
+        UserEntity user = userRepository.findUserById(id);
+        request.setUserId(user);
         farmRepository.save(request.toEntity());
     }
 
