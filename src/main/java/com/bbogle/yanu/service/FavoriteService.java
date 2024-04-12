d2b99558721572877a471548d2ce01977786a578
seonyo <jsy6535@naver.com>
2024-04-12 18:59:47 +0900

    :bug: 세션을 사용한 하트 등록 서비스 수정

--- src/main/java/com/bbogle/yanu/service/FavoriteService.java
+++ src/main/java/com/bbogle/yanu/service/FavoriteService.java
@@ -3,9 +3,14 @@ package com.bbogle.yanu.service;
 import com.bbogle.yanu.dto.favorite.DeleteHeartRequestDto;
 import com.bbogle.yanu.dto.favorite.RegisterHeartRequestDto;
 import com.bbogle.yanu.entity.FavoriteEntity;
+import com.bbogle.yanu.entity.UserEntity;
 import com.bbogle.yanu.exception.HeartDuplicateException;
+import com.bbogle.yanu.exception.SessionNotFoundException;
 import com.bbogle.yanu.exception.error.ErrorCode;
 import com.bbogle.yanu.repository.FavoriteRepository;
+import com.bbogle.yanu.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,9 +21,16 @@ import java.util.List;
 @Service
 public class FavoriteService {
     private final FavoriteRepository favoriteRepository;
+    private final UserRepository userRepository;
 
-    public void registerHeart(RegisterHeartRequestDto request){
-        Long userId = request.getUserId().getId();
+    public void registerHeart(RegisterHeartRequestDto request, HttpServletRequest httpRequest){
+        HttpSession session = httpRequest.getSession(false);
+
+        if(session == null){
+            throw new SessionNotFoundException("session not found", ErrorCode.SESSION_NOTFOUND);
+        }
+
+        Long userId = (Long) session.getAttribute("userId");
         Long productId = request.getProductId().getId();
         String type = request.getType();
         boolean exists = favoriteRepository.existsByUserIdAndProductIdAndType(userId, productId, type);
@@ -26,6 +38,9 @@ public class FavoriteService {
         if(exists){
             throw new HeartDuplicateException("heart duplicated", ErrorCode.HEART_DUPLICATION);
         }
+
+        UserEntity user = userRepository.findUserById(userId);
+        request.setUserId(user);
         favoriteRepository.save(request.toEntity());
     }
 
