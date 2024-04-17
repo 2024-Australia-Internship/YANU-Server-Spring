aed5bc042f47c3f5660c1b9917d1749e87a213de
seonyo <jsy6535@naver.com>
2024-04-17 21:58:06 +0900

    카트 관련 커밋내용 되돌리기

--- src/main/java/com/bbogle/yanu/controller/CartController.java
+++ src/main/java/com/bbogle/yanu/controller/CartController.java
@@ -1,16 +1,20 @@
 package com.bbogle.yanu.controller;
 
+import com.bbogle.yanu.dto.cart.DeleteCartDto;
+import com.bbogle.yanu.dto.cart.FindCartResponseDto;
 import com.bbogle.yanu.dto.cart.RegisterCartDto;
 import com.bbogle.yanu.dto.favorite.RegisterHeartRequestDto;
+import com.bbogle.yanu.entity.CartEntity;
+import com.bbogle.yanu.entity.UserEntity;
 import com.bbogle.yanu.service.CartService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,5 +26,19 @@ public class CartController {
     public ResponseEntity<String> registerCart(@RequestBody RegisterCartDto requst, HttpServletRequest httpRequest){
         cartService.registerCart(requst, httpRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body("장바구니 등록에 성공했습니다.");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteCart(@RequestBody DeleteCartDto request, HttpServletRequest httpRequest){
+        cartService.deleteCart(request, httpRequest);
+        return ResponseEntity.ok().body("장바구니에서 삭제 성공했습니다.");
+    }
+
+    @GetMapping
+    public List<FindCartResponseDto> findCart(HttpServletRequest httpRequest){
+        List<CartEntity> carts = cartService.findCart(httpRequest);
+        return carts.stream()
+                .map(FindCartResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
