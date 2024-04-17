09b50235ae7ae6e97c9683545ecd95e57fa20f4e
seonyo <jsy6535@naver.com>
2024-04-18 02:11:16 +0900

    :bug: S3Config Value 경로 오류 수정

--- src/main/java/com/bbogle/yanu/config/S3Config.java
+++ src/main/java/com/bbogle/yanu/config/S3Config.java
@@ -1,9 +1,7 @@
 package com.bbogle.yanu.config;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,17 +12,17 @@ import org.springframework.context.annotation.Configuration;
 public class S3Config {
     @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
-
     @Value("${cloud.aws.credentials.secret-key}")
-    private String accessSecret;
-    @Value("${cloud.aws.region.static}")
+    private String secretKey;
+    @Value("${cloud.regin.static}")
     private String region;
 
     @Bean
-    public AmazonS3 s3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, accessSecret);
-        return AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(region).build();
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials awsCredentials= new BasicAWSCredentials(accessKey, secretKey);
+        return (AmazonS3Client)AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
     }
 }
