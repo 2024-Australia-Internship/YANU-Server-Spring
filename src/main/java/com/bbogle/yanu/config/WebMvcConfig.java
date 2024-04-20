8cdd4e6d757345b15bdea87f5e5d02416db02d92
seonyo <jsy6535@naver.com>
2024-04-20 23:53:21 +0900

    :bug: Access-Control-Allow-Origin Cors 에러 해결

--- src/main/java/com/bbogle/yanu/config/WebMvcConfig.java
+++ src/main/java/com/bbogle/yanu/config/WebMvcConfig.java
@@ -1,18 +1,23 @@
 package com.bbogle.yanu.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:5500")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public WebMvcConfigurer addCorsMappings() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
+                        .exposedHeaders("Authorization", "RefreshToken");
+            }
+        };
     }
-
 }
