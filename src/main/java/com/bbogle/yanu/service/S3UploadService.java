053f43657d2fa5587d593e083dbfebbded7ff548
seonyo <jsy6535@naver.com>
2024-04-20 14:29:53 +0900

    ✨ 프로필 이미지 등록 서비스 이메일로 파일 이름 등록

--- src/main/java/com/bbogle/yanu/service/S3UploadService.java
+++ src/main/java/com/bbogle/yanu/service/S3UploadService.java
@@ -2,6 +2,13 @@ package com.bbogle.yanu.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.bbogle.yanu.entity.UserEntity;
+import com.bbogle.yanu.exception.EmailNotFoundException;
+import com.bbogle.yanu.exception.SessionNotFoundException;
+import com.bbogle.yanu.exception.error.ErrorCode;
+import com.bbogle.yanu.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -14,18 +21,22 @@ import java.util.UUID;
 @Service
 public class S3UploadService {
     private final AmazonS3 amazonS3;
+    private final UserRepository userRepository;
 
     @Value("${could.aws.s3.bucket}")
     private String bucketName;
 
-    public String uploadProfile(MultipartFile file) throws IOException {
-        String fileName = generateFileName(file);
+    public String uploadProfile(String email, MultipartFile file) throws IOException {
+        if(!userRepository.existsByEmail(email))
+            throw new EmailNotFoundException("email not found", ErrorCode.EMAIL_NOTFOUND);
+
+        String fileName = generateFileName(email, file);
         String fileURL = uploadFileToS3(fileName, file);
         return fileURL;
     }
 
-    private String generateFileName(MultipartFile file){
-        return UUID.randomUUID().toString() + "_" + "profile" + "_" + file.getOriginalFilename();
+    private String generateFileName(String email, MultipartFile file){
+        return UUID.randomUUID().toString() + "_" + "profile" + "_" + email + "_" + file.getOriginalFilename();
     }
 
     private String uploadFileToS3(String fileName, MultipartFile file) throws IOException{
@@ -35,4 +46,5 @@ public class S3UploadService {
         amazonS3.putObject(bucketName, fileName, file.getInputStream(), metadata);
         return amazonS3.getUrl(bucketName, fileName).toString();
     }
+
 }
