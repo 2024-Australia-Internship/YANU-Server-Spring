f83860eca8e5510bd760e9b42125a8b593fcdad6
seonyo <jsy6535@naver.com>
2024-04-27 20:59:34 +0900

    :bug: UserIdResponseDto profile_image 값 주입

--- src/main/java/com/bbogle/yanu/dto/user/UserIdResponseDto.java
+++ src/main/java/com/bbogle/yanu/dto/user/UserIdResponseDto.java
@@ -3,21 +3,19 @@ package com.bbogle.yanu.dto.user;
 import com.bbogle.yanu.entity.UserEntity;
 import lombok.Getter;
 
-import java.util.Optional;
-
 @Getter
 public class UserIdResponseDto {
     private String email;
     private String phonenumber;
     private String profile_image;
     private String nickname;
-    private String introduction;
     private Byte ugly_percent;
     private Boolean is_farmer;
 
-    public UserIdResponseDto(UserEntity userEntity) {
+    public UserIdResponseDto(UserEntity userEntity, String profile_image) {
         this.email = userEntity.getEmail();
         this.phonenumber = userEntity.getPhonenumber();
+        this.profile_image = profile_image;
         this.nickname = userEntity.getNickname();
         this.ugly_percent = userEntity.getUgly_percent();
         this.is_farmer = userEntity.getIs_farmer();
