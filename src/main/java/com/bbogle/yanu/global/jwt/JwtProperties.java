701b86ebfb31f508cc69dcc0f93d3648c10c3437
seonyo <jsy6535@naver.com>
2024-05-21 01:29:13 +0900

    ✨ (#29) JwtProperties 추가

---
+++ src/main/java/com/bbogle/yanu/global/jwt/JwtProperties.java
@@ -0,0 +1,14 @@
+    package com.bbogle.yanu.global.jwt;
+
+    import lombok.Getter;
+    import lombok.Setter;
+    import org.springframework.boot.context.properties.ConfigurationProperties;
+    import org.springframework.stereotype.Component;
+
+    @Setter @Getter
+    @Component
+    @ConfigurationProperties("jwt")
+    public class JwtProperties {
+        private String issuer;
+        private String secretKey;
+    }
