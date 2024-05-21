fc7018a3e1dd8faf29087d3d04eb1df5e046f652
seonyo <jsy6535@naver.com>
2024-05-22 02:37:04 +0900

    ♻ (#28) Search 서비스 컨트롤러 기능별로 분리

--- src/main/java/com/bbogle/yanu/domain/search/controller/SearchController.java
+++ src/main/java/com/bbogle/yanu/domain/search/controller/SearchController.java
@@ -3,7 +3,9 @@ package com.bbogle.yanu.domain.search.controller;
 import com.bbogle.yanu.domain.search.dto.SearchResponseDto;
 import com.bbogle.yanu.domain.search.dto.SearchTypeResponseDto;
 import com.bbogle.yanu.domain.product.domain.ProductEntity;
-import com.bbogle.yanu.domain.search.service.SearchService;
+import com.bbogle.yanu.domain.search.service.SearchProductService;
+import com.bbogle.yanu.domain.search.service.SearchTypeProductService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,19 +19,20 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @RequestMapping("/searches")
 public class SearchController {
-    private final SearchService searchService;
+    private final SearchProductService searchProductService;
+    private final SearchTypeProductService searchTypeProductService;
 
     @GetMapping("/{keyword}")
-    public List<SearchResponseDto> searchProduct(@PathVariable ("keyword") String keyword){
-        List<ProductEntity> products = searchService.searchProduct(keyword);
+    public List<SearchResponseDto> searchProduct(@PathVariable ("keyword") String keyword, HttpServletRequest httpRequest){
+        List<ProductEntity> products = searchProductService.execute(keyword, httpRequest);
         return products.stream()
                 .map(SearchResponseDto::new)
                 .collect(Collectors.toList());
     }
 
     @GetMapping("/{keyword}/{type}")
-    public List<SearchTypeResponseDto> searchTypeProduct(@PathVariable ("keyword") String keyword, @PathVariable("type") String type){
-        List<ProductEntity> products = searchService.searchTypeProduct(keyword, type);
+    public List<SearchTypeResponseDto> searchTypeProduct(@PathVariable ("keyword") String keyword, @PathVariable("type") String type, HttpServletRequest httpRequest){
+        List<ProductEntity> products = searchTypeProductService.execute(keyword, type, httpRequest);
         return products.stream()
                 .map(SearchTypeResponseDto::new)
                 .collect(Collectors.toList());
---
+++ src/main/java/com/bbogle/yanu/domain/search/service/SearchTypeProductService.java
@@ -0,0 +1,34 @@
+package com.bbogle.yanu.domain.search.service;
+
+import com.bbogle.yanu.domain.product.domain.ProductEntity;
+import com.bbogle.yanu.domain.search.dto.SearchTypeResponseDto;
+import com.bbogle.yanu.domain.search.repository.SearchRepository;
+import com.bbogle.yanu.global.exception.ProductNotFoundException;
+import com.bbogle.yanu.global.exception.TokenNotFoundException;
+import com.bbogle.yanu.global.exception.error.ErrorCode;
+import com.bbogle.yanu.global.jwt.TokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class SearchTypeProductService {
+    private final SearchRepository searchRepository;
+    private final TokenProvider tokenProvider;
+
+    public List<ProductEntity> execute(String keyword, String type, HttpServletRequest httpRequest) {
+        String token = tokenProvider.resolveToken(httpRequest);
+        if (token == null || !tokenProvider.validToken(token)) {
+            throw new TokenNotFoundException("Invalid token", ErrorCode.TOKEN_NOTFOUND);
+        }
+
+        List<ProductEntity> searchResult = searchRepository.findAllByTitleContaining(keyword);
+        if (searchResult.isEmpty()) {
+            throw new ProductNotFoundException("product not found", ErrorCode.PRODUCT_NOTFOUND);
+        }
+        return searchResult;
+    }
+}
