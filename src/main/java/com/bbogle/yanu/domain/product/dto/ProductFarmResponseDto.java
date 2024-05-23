1ecb4284324f09987b92a913ec65dcd6bd741fa0
seonyo <jsy6535@naver.com>
2024-05-23 12:07:31 +0900

    :bug: 상품 조회 누락된 데이터 추가

--- src/main/java/com/bbogle/yanu/domain/product/dto/ProductFarmResponseDto.java
+++ src/main/java/com/bbogle/yanu/domain/product/dto/ProductFarmResponseDto.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 
 @Getter
 public class ProductFarmResponseDto {
+    private Long userId;
+    private Long farmId;
+    private String farm_name;
     private String title;
     private String category;
     private String hashtag;
@@ -13,6 +16,9 @@ public class ProductFarmResponseDto {
     private String description;
 
     public ProductFarmResponseDto(ProductEntity productEntity) {
+        this.userId = productEntity.getFarm().getUser().getId();
+        this.farmId = productEntity.getFarm().getId();
+        this.farm_name = productEntity.getFarm().getFarm_name();
         this.title = productEntity.getTitle();
         this.category = productEntity.getCategory();
         this.hashtag = productEntity.getHashtag();
