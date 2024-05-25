252a932cf24fa2f667bc97ec3b846a2c6880524e
seonyo <jsy6535@naver.com>
2024-05-26 02:44:03 +0900

    ✨ Id로 Email 찾는 jpql 구현

--- src/main/java/com/bbogle/yanu/domain/user/repository/UserRepository.java
+++ src/main/java/com/bbogle/yanu/domain/user/repository/UserRepository.java
@@ -2,10 +2,14 @@ package com.bbogle.yanu.domain.user.repository;
 
 import com.bbogle.yanu.domain.user.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByEmail(String email);
     boolean existsByPhonenumber(String phonenumber);
     UserEntity findByEmail(String email);
     UserEntity findUserById(Long id);
+    @Query("SELECT u.email FROM users u WHERE u.id = :userId")
+    String findEmailById(@Param("userId") Long userId);
 }
