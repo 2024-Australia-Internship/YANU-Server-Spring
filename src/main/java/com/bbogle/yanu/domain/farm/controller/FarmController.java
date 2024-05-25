ca860444340534d18ff771a4a2b7c184caafd508
seonyo <jsy6535@naver.com>
2024-05-26 03:42:45 +0900

    ✨ 농장 정보 등록하는 컨트롤러 구현

--- src/main/java/com/bbogle/yanu/domain/farm/controller/FarmController.java
+++ src/main/java/com/bbogle/yanu/domain/farm/controller/FarmController.java
@@ -6,17 +6,22 @@ import com.bbogle.yanu.domain.farm.domain.FarmEntity;
 import com.bbogle.yanu.domain.farm.service.FarmRegisterService;
 import com.bbogle.yanu.domain.farm.service.FindMyFarmInfoService;
 import com.bbogle.yanu.domain.farm.service.FindOtherFarmInfoService;
+import com.bbogle.yanu.domain.farm.service.FarmImageRegisterService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/farms")
 public class FarmController {
     private final FarmRegisterService farmRegisterService;
+    private final FarmImageRegisterService farmImageRegisterService;
     private final FindMyFarmInfoService findMyFarmInfoService;
     private final FindOtherFarmInfoService findOtherFarmInfoService;
 
@@ -24,6 +29,13 @@ public class FarmController {
     public ResponseEntity<String> registerFarm(@RequestBody RegisterFarmRequestDto request, HttpServletRequest httpRequest){
         farmRegisterService.execute(request, httpRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body("농장 등록 성공했습니다.");
+    }
+
+    @PostMapping("/image")
+    public ResponseEntity<String> registerFarmImage(@RequestParam("profile")MultipartFile file,
+                                                    HttpServletRequest httpRequest) throws IOException {
+        farmImageRegisterService.execute(file, httpRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body("농장 프로필 등록 성공했습니다");
     }
 
     @GetMapping
