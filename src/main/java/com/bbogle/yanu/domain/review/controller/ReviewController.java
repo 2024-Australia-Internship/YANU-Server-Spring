223ea4526dfe4725a65bcb5bd56804b2ce84ec94
seonyo <jsy6535@naver.com>
2024-05-27 02:48:30 +0900

    ✨ 리뷰 등록 컨트롤러 구현

--- src/main/java/com/bbogle/yanu/domain/review/controller/ReviewController.java
+++ src/main/java/com/bbogle/yanu/domain/review/controller/ReviewController.java
@@ -1,6 +1,13 @@
 package com.bbogle.yanu.domain.review.controller;
 
+import com.bbogle.yanu.domain.review.dto.CreateReviewRequestDto;
+import com.bbogle.yanu.domain.review.service.CreateReviewService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/reviews")
 public class ReviewController {
+    private final CreateReviewService createReviewService;
+
+    @PostMapping
+    public ResponseEntity<String> createReview (@RequestBody CreateReviewRequestDto request, HttpServletRequest httpRequest){
+        createReviewService.execute(request, httpRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body("리뷰 등록 성공했습니다");
+    }
 }
