220c86847398279e27ced702f2036452ede207dd
seonyo <jsy6535@naver.com>
2024-05-27 02:54:46 +0900

    ✨ existsByUserIdAndProductId 메소드 추가

--- src/main/java/com/bbogle/yanu/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/bbogle/yanu/domain/review/repository/ReviewRepository.java
@@ -4,4 +4,5 @@ import com.bbogle.yanu.domain.review.domain.ReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
+    boolean existsByUserIdAndProductId(Long userId, Long productId);
 }
