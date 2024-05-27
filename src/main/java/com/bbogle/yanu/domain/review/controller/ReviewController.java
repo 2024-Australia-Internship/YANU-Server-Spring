c0e55ad3a07aa3701acabb12d6fe6c7a926fe36e
seonyo <jsy6535@naver.com>
2024-05-27 21:08:34 +0900

    ✨ 특정 회원 리뷰 조회 컨트롤러 구현

--- src/main/java/com/bbogle/yanu/domain/review/controller/ReviewController.java
+++ src/main/java/com/bbogle/yanu/domain/review/controller/ReviewController.java
@@ -3,8 +3,10 @@ package com.bbogle.yanu.domain.review.controller;
 import com.bbogle.yanu.domain.review.domain.ReviewEntity;
 import com.bbogle.yanu.domain.review.dto.CreateReviewRequestDto;
 import com.bbogle.yanu.domain.review.dto.FindMyReveiwResponseDto;
+import com.bbogle.yanu.domain.review.dto.FindOtherReviewResponseDto;
 import com.bbogle.yanu.domain.review.service.CreateReviewService;
 import com.bbogle.yanu.domain.review.service.FindMyReviewService;
+import com.bbogle.yanu.domain.review.service.FindOtherReviewService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,6 +22,7 @@ import java.util.stream.Collectors;
 public class ReviewController {
     private final CreateReviewService createReviewService;
     private final FindMyReviewService findMyReviewService;
+    private final FindOtherReviewService findOtherReviewService;
 
     @PostMapping
     public ResponseEntity<String> createReview (@RequestBody CreateReviewRequestDto request, HttpServletRequest httpRequest){
@@ -32,6 +35,14 @@ public class ReviewController {
         List<ReviewEntity> reviews = findMyReviewService.execute(httpRequest);
         return reviews.stream()
                 .map(FindMyReveiwResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{user_id}")
+    public List<FindOtherReviewResponseDto> findOtherReview(@PathVariable("user_id") Long userId, HttpServletRequest httpRequest){
+        List<ReviewEntity> reviews = findOtherReviewService.execute(userId, httpRequest);
+        return reviews.stream()
+                .map(FindOtherReviewResponseDto::new)
                 .collect(Collectors.toList());
     }
 }
