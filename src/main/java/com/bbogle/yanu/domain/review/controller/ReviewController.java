0499f115c7abd3de455d19be6143c8819134e519
seonyo <jsy6535@naver.com>
2024-05-27 20:55:35 +0900

    ✨ 나의 리뷰 내역 불러오는 컨트롤러 구현

--- src/main/java/com/bbogle/yanu/domain/review/controller/ReviewController.java
+++ src/main/java/com/bbogle/yanu/domain/review/controller/ReviewController.java
@@ -1,25 +1,37 @@
 package com.bbogle.yanu.domain.review.controller;
 
+import com.bbogle.yanu.domain.review.domain.ReviewEntity;
 import com.bbogle.yanu.domain.review.dto.CreateReviewRequestDto;
+import com.bbogle.yanu.domain.review.dto.FindMyReveiwResponseDto;
 import com.bbogle.yanu.domain.review.service.CreateReviewService;
+import com.bbogle.yanu.domain.review.service.FindMyReviewService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/reviews")
 public class ReviewController {
     private final CreateReviewService createReviewService;
+    private final FindMyReviewService findMyReviewService;
 
     @PostMapping
     public ResponseEntity<String> createReview (@RequestBody CreateReviewRequestDto request, HttpServletRequest httpRequest){
         createReviewService.execute(request, httpRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body("리뷰 등록 성공했습니다");
+    }
+
+    @GetMapping
+    public List<FindMyReveiwResponseDto> findMyReview(HttpServletRequest httpRequest){
+        List<ReviewEntity> reviews = findMyReviewService.execute(httpRequest);
+        return reviews.stream()
+                .map(FindMyReveiwResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
