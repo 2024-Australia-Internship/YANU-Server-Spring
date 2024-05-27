c8d2938397fc3d01b6890ac31f15187c0baa2d5b
seonyo <jsy6535@naver.com>
2024-05-27 21:44:20 +0900

    ✨ findByUserIdAndProductId 메소드 추가

--- src/main/java/com/bbogle/yanu/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/bbogle/yanu/domain/review/repository/ReviewRepository.java
@@ -4,8 +4,10 @@ import com.bbogle.yanu.domain.review.domain.ReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
     boolean existsByUserIdAndProductId(Long userId, Long productId);
     List<ReviewEntity> findAllByUserId(Long userId);
+    Optional<ReviewEntity> findByUserIdAndProductId(Long userId, Long productId);
 }
