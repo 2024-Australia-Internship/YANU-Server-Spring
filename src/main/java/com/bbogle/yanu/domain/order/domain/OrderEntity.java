187220dc4c0e734585fc7fdb1069b63c10156df5
seonyo <jsy6535@naver.com>
2024-05-27 23:44:25 +0900

    🗃️ OrderEntity 생성

---
+++ src/main/java/com/bbogle/yanu/domain/order/domain/OrderEntity.java
@@ -0,0 +1,34 @@
+package com.bbogle.yanu.domain.order.domain;
+
+import com.bbogle.yanu.domain.product.domain.ProductEntity;
+import com.bbogle.yanu.domain.user.domain.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class OrderEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="product_id")
+    private ProductEntity product;
+
+    @Column
+    @CreationTimestamp
+    private LocalDate order_date;
+
+
+}
