16ff05f043581e6eea918632c367df5fb197174d
seonyo <jsy6535@naver.com>
2024-05-30 03:29:46 +0900

    ✨ 주문내역 조회 컨트롤러 구현

--- src/main/java/com/bbogle/yanu/domain/order/controller/OrderController.java
+++ src/main/java/com/bbogle/yanu/domain/order/controller/OrderController.java
@@ -1,24 +1,37 @@
 package com.bbogle.yanu.domain.order.controller;
 
+import com.bbogle.yanu.domain.order.domain.OrderEntity;
+import com.bbogle.yanu.domain.order.dto.FindOrderResponseDto;
 import com.bbogle.yanu.domain.order.dto.OrderRequestDto;
+import com.bbogle.yanu.domain.order.service.FindOrderService;
 import com.bbogle.yanu.domain.order.service.OrderService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/orders")
 public class OrderController {
     private final OrderService orderService;
+    private final FindOrderService findOrderService;
+
     @PostMapping
     public ResponseEntity<String> order(@RequestBody OrderRequestDto request, HttpServletRequest httpRequest){
         orderService.execute(request, httpRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body("주문 성공 했습니다");
+    }
+
+    @GetMapping
+    public List<FindOrderResponseDto> findOrder(HttpServletRequest httpRequest){
+        List<OrderEntity> orders = findOrderService.execute(httpRequest);
+        return orders.stream()
+                .map(FindOrderResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
