7fec8333ba7cb2c2b2564cda4cfde70785dae7d5
seonyo <jsy6535@naver.com>
2024-05-30 04:12:53 +0900

    ✨ 농부 하트 등록 컨트롤러 구현

--- src/main/java/com/bbogle/yanu/domain/favorite/farm/controller/FavoriteFarmController.java
+++ src/main/java/com/bbogle/yanu/domain/favorite/farm/controller/FavoriteFarmController.java
@@ -1,4 +1,26 @@
 package com.bbogle.yanu.domain.favorite.farm.controller;
 
+import com.bbogle.yanu.domain.favorite.farm.dto.RegisterFarmHeartRequestDto;
+import com.bbogle.yanu.domain.favorite.farm.service.RegisterFarmHeartService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/favorites/farms")
 public class FavoriteFarmController {
+    private final RegisterFarmHeartService registerHeartService;
+
+    @PostMapping
+    public ResponseEntity<String> registerHeart(@RequestBody RegisterFarmHeartRequestDto request, HttpServletRequest httpRequest){
+        registerHeartService.execute(request, httpRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body("하트 등록에 성공했습니다.");
+    }
+
 }
