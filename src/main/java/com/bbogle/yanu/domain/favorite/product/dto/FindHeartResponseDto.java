e4bdca0aae57d59d24b2ebe6d709e414e24a0389
seonyo <jsy6535@naver.com>
2024-05-30 03:57:57 +0900

    :fire: FindHeartResponseDto type 관련 코드 제거

--- src/main/java/com/bbogle/yanu/domain/favorite/product/dto/FindHeartResponseDto.java
+++ src/main/java/com/bbogle/yanu/domain/favorite/product/dto/FindHeartResponseDto.java
@@ -7,11 +7,9 @@ import lombok.Getter;
 public class FindHeartResponseDto {
     private Long user_id;
     private Long product_id;
-    private String type;
 
     public FindHeartResponseDto(FavoriteProductEntity favoriteEntity) {
         this.user_id = favoriteEntity.getUser().getId();
         this.product_id = favoriteEntity.getProduct().getId();
-        this.type = favoriteEntity.getType();
     }
 }
