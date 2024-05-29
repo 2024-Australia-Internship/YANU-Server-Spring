c496e9e7dcd12200ff41656be231aca7b40c9fe5
seonyo <jsy6535@naver.com>
2024-05-30 04:12:30 +0900

    ✨ FavoriteFarmRepository 설정

--- src/main/java/com/bbogle/yanu/domain/favorite/farm/repository/FavoriteFarmRepository.java
+++ src/main/java/com/bbogle/yanu/domain/favorite/farm/repository/FavoriteFarmRepository.java
@@ -1,4 +1,11 @@
 package com.bbogle.yanu.domain.favorite.farm.repository;
 
-public interface FavoriteFarmRepository {
+import com.bbogle.yanu.domain.favorite.farm.domain.FavoriteFarmEntity;
+import com.bbogle.yanu.domain.favorite.product.domain.FavoriteProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FavoriteFarmRepository extends JpaRepository <FavoriteFarmEntity, Long> {
+    boolean existsByUserIdAndFarmId(Long userId, Long farmId);
 }
