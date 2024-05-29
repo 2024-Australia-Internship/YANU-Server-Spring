58bcbb84bc171ab452df52b5bf8b3511f06c1052
seonyo <jsy6535@naver.com>
2024-05-30 03:29:27 +0900

    ✨ findAllByUserId 메소드 추가

--- src/main/java/com/bbogle/yanu/domain/order/repository/OrderRepository.java
+++ src/main/java/com/bbogle/yanu/domain/order/repository/OrderRepository.java
@@ -3,5 +3,8 @@ package com.bbogle.yanu.domain.order.repository;
 import com.bbogle.yanu.domain.order.domain.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+    List<OrderEntity> findAllByUserId(Long userId);
 }
