1a8a735a85187fae2aac25a6315c1c6b3a14661e
seonyo <jsy6535@naver.com>
2024-05-30 04:09:58 +0900

    ✨ FavoriteFarmEntity 생성

--- src/main/java/com/bbogle/yanu/domain/favorite/farm/domain/FavoriteFarmEntity.java
+++ src/main/java/com/bbogle/yanu/domain/favorite/farm/domain/FavoriteFarmEntity.java
@@ -1,4 +1,26 @@
 package com.bbogle.yanu.domain.favorite.farm.domain;
 
+import com.bbogle.yanu.domain.farm.domain.FarmEntity;
+import com.bbogle.yanu.domain.product.domain.ProductEntity;
+import com.bbogle.yanu.domain.user.domain.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "favorite_farms")
 public class FavoriteFarmEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "farm_id")
+    private FarmEntity farm;
 }
