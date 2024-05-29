25731597012ead687107334519aa140b3b636aa9
seonyo <jsy6535@naver.com>
2024-05-30 04:17:57 +0900

    ✨ 농장 하트 삭제 컨트롤러 구현

--- src/main/java/com/bbogle/yanu/domain/favorite/farm/controller/FavoriteFarmController.java
+++ src/main/java/com/bbogle/yanu/domain/favorite/farm/controller/FavoriteFarmController.java
@@ -1,26 +1,33 @@
 package com.bbogle.yanu.domain.favorite.farm.controller;
 
+import com.bbogle.yanu.domain.favorite.farm.dto.DeleteFarmHeartRequestDto;
 import com.bbogle.yanu.domain.favorite.farm.dto.RegisterFarmHeartRequestDto;
+import com.bbogle.yanu.domain.favorite.farm.service.DeleteFarmHeartService;
 import com.bbogle.yanu.domain.favorite.farm.service.RegisterFarmHeartService;
+import com.bbogle.yanu.domain.favorite.product.dto.DeleteHeartRequestDto;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/favorites/farms")
 public class FavoriteFarmController {
-    private final RegisterFarmHeartService registerHeartService;
+    private final RegisterFarmHeartService registerFarmHeartService;
+    private final DeleteFarmHeartService deleteFarmHeartService;
 
     @PostMapping
     public ResponseEntity<String> registerHeart(@RequestBody RegisterFarmHeartRequestDto request, HttpServletRequest httpRequest){
-        registerHeartService.execute(request, httpRequest);
+        registerFarmHeartService.execute(request, httpRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body("하트 등록에 성공했습니다.");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteHeart(@RequestBody DeleteFarmHeartRequestDto request, HttpServletRequest httpRequest){
+        deleteFarmHeartService.execute(request, httpRequest);
+        return ResponseEntity.ok().body("하트 삭제 성공했습니다.");
     }
 
 }
