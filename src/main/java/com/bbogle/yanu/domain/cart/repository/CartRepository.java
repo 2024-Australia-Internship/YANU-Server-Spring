9531e285278388f94a3d460522b903960d135097
seonyo <jsy6535@naver.com>
2024-05-30 11:10:36 +0900

    ✨ findByUserIdAndProductId 메소드 추가

--- src/main/java/com/bbogle/yanu/domain/cart/repository/CartRepository.java
+++ src/main/java/com/bbogle/yanu/domain/cart/repository/CartRepository.java
@@ -4,10 +4,11 @@ import com.bbogle.yanu.domain.cart.domain.CartEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<CartEntity, Long> {
     boolean existsByUserIdAndProductId(Long userId, Long productId);
     void deleteByUserIdAndProductId(Long userId, Long productId);
-
     List<CartEntity> findAllByUserId(Long userId);
+    Optional<CartEntity> findByUserIdAndProductId(Long userId, Long productId);
 }
