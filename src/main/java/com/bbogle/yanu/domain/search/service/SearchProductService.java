2c5e0450d90b80239035405e56c10673ee897315
seonyo <jsy6535@naver.com>
2024-06-08 14:11:48 +0900

    ✨  상품 검색 시 하트 여부 response data 추가

--- src/main/java/com/bbogle/yanu/domain/search/service/SearchProductService.java
+++ src/main/java/com/bbogle/yanu/domain/search/service/SearchProductService.java
@@ -1,6 +1,8 @@
 package com.bbogle.yanu.domain.search.service;
 
 import com.bbogle.yanu.domain.product.domain.ProductEntity;
+import com.bbogle.yanu.domain.product.facade.ProductFacade;
+import com.bbogle.yanu.domain.search.dto.SearchProductResponseDto;
 import com.bbogle.yanu.domain.search.repository.SearchRepository;
 import com.bbogle.yanu.global.exception.ProductNotFoundException;
 import com.bbogle.yanu.global.exception.TokenNotFoundException;
@@ -10,23 +12,33 @@ import com.bbogle.yanu.global.jwt.TokenValidator;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class SearchProductService {
     private final SearchRepository searchRepository;
+    private final ProductFacade productFacade;
     private final TokenValidator tokenValidator;
+    private final TokenProvider tokenProvider;
 
-    public List<ProductEntity> execute(String keyword, HttpServletRequest httpRequest){
+    @Transactional(readOnly = true)
+    public List<SearchProductResponseDto> execute(String keyword, HttpServletRequest httpRequest){
         String token = tokenValidator.validateToken(httpRequest);
+
+        Long userId = tokenProvider.getUserId(token);
 
         List<ProductEntity> searchResult = searchRepository.findAllByTitleContaining(keyword);
         if (searchResult.isEmpty()) {
             throw new ProductNotFoundException("product not found", ErrorCode.PRODUCT_NOTFOUND);
         }
-        return searchResult;
+
+        return searchResult.stream()
+                .map(result -> new SearchProductResponseDto(result, productFacade.checkIsHeart(result, userId)))
+                .collect(Collectors.toList());
 
     }
 }
--- src/main/java/com/bbogle/yanu/domain/search/service/SearchTypeProductService.java
+++ src/main/java/com/bbogle/yanu/domain/search/service/SearchTypeProductService.java
@@ -1,6 +1,9 @@
 package com.bbogle.yanu.domain.search.service;
 
 import com.bbogle.yanu.domain.product.domain.ProductEntity;
+import com.bbogle.yanu.domain.product.facade.ProductFacade;
+import com.bbogle.yanu.domain.search.dto.SearchProductResponseDto;
+import com.bbogle.yanu.domain.search.dto.SearchResponseDto;
 import com.bbogle.yanu.domain.search.dto.SearchTypeResponseDto;
 import com.bbogle.yanu.domain.search.repository.SearchRepository;
 import com.bbogle.yanu.global.exception.ProductNotFoundException;
@@ -13,20 +16,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class SearchTypeProductService {
     private final SearchRepository searchRepository;
+    private final ProductFacade productFacade;
     private final TokenValidator tokenValidator;
+    private final TokenProvider tokenProvider;
 
-    public List<ProductEntity> execute(String keyword, String type, HttpServletRequest httpRequest) {
+    public List<SearchResponseDto> execute(String keyword, String type, HttpServletRequest httpRequest) {
         String token = tokenValidator.validateToken(httpRequest);
+
+        Long userId = tokenProvider.getUserId(token);
 
         List<ProductEntity> searchResult = searchRepository.findAllByTitleContainingAndCategory(keyword, type);
         if (searchResult.isEmpty()) {
             throw new ProductNotFoundException("product not found", ErrorCode.PRODUCT_NOTFOUND);
         }
-        return searchResult;
+        return searchResult.stream()
+                .map(result -> new SearchResponseDto(result, productFacade.checkIsHeart(result, userId)))
+                .collect(Collectors.toList());
     }
 }
