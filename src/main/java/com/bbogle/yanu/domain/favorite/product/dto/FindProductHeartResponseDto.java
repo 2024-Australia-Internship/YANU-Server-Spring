7b4843a12a6cc29cf16f2ee500daf6c124da714e
seonyo <jsy6535@naver.com>
2024-06-08 13:56:39 +0900

    ✨ 하트 조회 response data 추가

--- src/main/java/com/bbogle/yanu/domain/favorite/product/dto/FindProductHeartResponseDto.java
+++ src/main/java/com/bbogle/yanu/domain/favorite/product/dto/FindProductHeartResponseDto.java
@@ -7,9 +7,15 @@ import lombok.Getter;
 public class FindProductHeartResponseDto {
     private Long user_id;
     private Long product_id;
+    private String title;
+    private int price;
+    private String unit;
 
     public FindProductHeartResponseDto(FavoriteProductEntity favoriteEntity) {
         this.user_id = favoriteEntity.getUser().getId();
         this.product_id = favoriteEntity.getProduct().getId();
+        this.title = favoriteEntity.getProduct().getTitle();
+        this.price = favoriteEntity.getProduct().getPrice();
+        this.unit = favoriteEntity.getProduct().getUnit();
     }
 }
