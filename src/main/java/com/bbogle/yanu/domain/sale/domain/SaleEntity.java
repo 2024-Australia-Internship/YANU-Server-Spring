c75ef7276f02a6bee3e0bea11aa2113ab2f1f972
seonyo <jsy6535@naver.com>
2024-06-10 14:02:21 +0900

    🗃️ SaleEntity 추가

---
+++ src/main/java/com/bbogle/yanu/domain/sale/domain/SaleEntity.java
@@ -0,0 +1,33 @@
+package com.bbogle.yanu.domain.sale.domain;
+
+import com.bbogle.yanu.domain.farm.domain.FarmEntity;
+import com.bbogle.yanu.domain.user.domain.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "sales")
+public class SaleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "farm_id")
+    private FarmEntity farm;
+
+    private int quantity;
+
+    @CreationTimestamp
+    private LocalDate sale_date;
+}
