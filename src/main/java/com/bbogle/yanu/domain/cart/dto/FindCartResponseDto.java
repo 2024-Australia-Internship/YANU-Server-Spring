c95d011eaf56aec419a980655f2dd90c86ddff9f
seonyo <jsy6535@naver.com>
2024-06-11 03:19:12 +0900

    :bug: cart response data 추가 변경

--- src/main/java/com/bbogle/yanu/domain/cart/dto/FindCartResponseDto.java
+++ src/main/java/com/bbogle/yanu/domain/cart/dto/FindCartResponseDto.java
@@ -8,10 +8,20 @@ public class FindCartResponseDto {
     private Long userId;
     private Long productId;
     private int quantity;
+    private String title;
+    private String businessName;
+    private String description;
+    private int price;
+    private String unit;
 
     public FindCartResponseDto(CartEntity cartEntity) {
         this.userId = cartEntity.getUser().getId();
         this.productId = cartEntity.getProduct().getId();
         this.quantity = cartEntity.getQuantity();
+        this.title = cartEntity.getProduct().getTitle();
+        this.businessName = cartEntity.getProduct().getFarm().getBusinessName();
+        this.description = cartEntity.getProduct().getDescription();
+        this.price = cartEntity.getProduct().getPrice();
+        this.unit = cartEntity.getProduct().getUnit();
     }
 }
