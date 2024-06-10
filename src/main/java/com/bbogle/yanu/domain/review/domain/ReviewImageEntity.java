3983f06cd3a8a309e27e1004ed8d978bbba2f8bc
seonyo <jsy6535@naver.com>
2024-06-11 03:59:42 +0900

    🗃️ ReviewImageEntity 추가

---
+++ src/main/java/com/bbogle/yanu/domain/review/domain/ReviewImageEntity.java
@@ -0,0 +1,21 @@
+package com.bbogle.yanu.domain.review.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "review_images")
+public class ReviewImageEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private ReviewEntity review;
+
+    private String url;
+}
