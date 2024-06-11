066ca5c3211830328a0366e5e445b196166684f2
seonyo <jsy6535@naver.com>
2024-06-12 01:37:28 +0900

    ✨ 리뷰 조회시 상품 정보 넘겨주기

--- src/main/java/com/bbogle/yanu/domain/review/dto/FindOtherReviewResponseDto.java
+++ src/main/java/com/bbogle/yanu/domain/review/dto/FindOtherReviewResponseDto.java
@@ -17,6 +17,9 @@ public class FindOtherReviewResponseDto {
     private String content;
     private LocalDate createdAt;
     private List<String> images;
+    private String writerName;
+    private String title;
+    private String description;
 
     public FindOtherReviewResponseDto (ReviewEntity reviewEntity, List<ReviewImageEntity> images){
         this.userId = reviewEntity.getUser().getId();
@@ -29,5 +32,8 @@ public class FindOtherReviewResponseDto {
                 .filter(image -> image.getReview().getId().equals(reviewEntity.getId()))
                 .map(ReviewImageEntity::getUrl)
                 .collect(Collectors.toList());
+        this.writerName = reviewEntity.getUser().getNickname();
+        this.title = reviewEntity.getProduct().getTitle();
+        this.description = reviewEntity.getProduct().getDescription();
     }
 }
