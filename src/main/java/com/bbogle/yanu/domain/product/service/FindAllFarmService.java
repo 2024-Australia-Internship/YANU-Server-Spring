a635a141b906c1fa7b59fd7b96aae8923389a61c
seonyo <jsy6535@naver.com>
2024-06-11 13:45:25 +0900

    :bug: FindAllFarmService 어노테이션 추가

--- src/main/java/com/bbogle/yanu/domain/product/service/FindAllFarmService.java
+++ src/main/java/com/bbogle/yanu/domain/product/service/FindAllFarmService.java
@@ -16,6 +16,7 @@ import com.bbogle.yanu.global.jwt.TokenValidator;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,6 +31,7 @@ public class FindAllFarmService {
     private final TokenValidator tokenValidator;
     private final TokenProvider tokenProvider;
 
+    @Transactional(readOnly = true)
     public List<ProductFarmResponseDto> execute(Long farmId, HttpServletRequest httpRequest){
         String token = tokenValidator.validateToken(httpRequest);
 
