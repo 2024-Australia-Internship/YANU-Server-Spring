7c89365fda9c9976f8ef1761772b63eaf6f1e9a3
seonyo <jsy6535@naver.com>
2024-06-13 00:42:54 +0900

    :bug: response 값 수정

--- src/main/java/com/bbogle/yanu/domain/farm/dto/FindListFarmResponseDto.java
+++ src/main/java/com/bbogle/yanu/domain/farm/dto/FindListFarmResponseDto.java
@@ -1,21 +1,36 @@
 package com.bbogle.yanu.domain.farm.dto;
 
 import com.bbogle.yanu.domain.farm.domain.FarmEntity;
+import com.bbogle.yanu.domain.product.domain.ProductEntity;
+import com.bbogle.yanu.domain.review.domain.ReviewEntity;
 import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class FindListFarmResponseDto {
+    private Long userId;
     private Long farmId;
     private Double latitude;
     private Double longitude;
     private String businessName;
-    private  float averageStarRating;
+    private String farmName;
+    private float averageStarRating;
+    private boolean checkIsHeart;
+    private List<Review> reviews;
+    private List<Product> products;
 
-    public FindListFarmResponseDto(FarmEntity farm, float averageStarRating){
+    public FindListFarmResponseDto(FarmEntity farm, float averageStarRating, List<ReviewEntity> reviews, List<ProductEntity> products, boolean checkIsHeart) {
+        this.userId = farm.getUser().getId();
         this.farmId = farm.getId();
         this.latitude = farm.getGeography().getX();
         this.longitude = farm.getGeography().getY();
         this.businessName = farm.getBusinessName();
+        this.farmName = farm.getFarm_name();
         this.averageStarRating = averageStarRating;
+        this.checkIsHeart = checkIsHeart;
+        this.reviews = reviews.stream().map(Review::new).collect(Collectors.toList());
+        this.products = products.stream().map(Product::new).collect(Collectors.toList());
     }
 }
