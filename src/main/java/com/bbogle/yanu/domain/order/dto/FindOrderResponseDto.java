0d6bfdd2c6dd86808c68f6cdb18006d1a8d66463
seonyo <jsy6535@naver.com>
2024-06-18 01:26:52 +0900

    ✨ 주문 내역 조회 시 orderId도 같이 넘겨주기

--- src/main/java/com/bbogle/yanu/domain/order/dto/FindOrderResponseDto.java
+++ src/main/java/com/bbogle/yanu/domain/order/dto/FindOrderResponseDto.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 
 @Getter
 public class FindOrderResponseDto {
+    private Long orderId;
     private Long userId;
     private Long farmId;
     private Long productId;
@@ -24,6 +25,7 @@ public class FindOrderResponseDto {
     private List<String> productImages;
 
     public FindOrderResponseDto(OrderEntity orderEntity, boolean isReview, List<ProductImageEntity> images){
+        this.orderId = orderEntity.getId();
         this.userId = orderEntity.getUser().getId();
         this.farmId = orderEntity.getProduct().getFarm().getId();
         this.productId = orderEntity.getProduct().getId();
