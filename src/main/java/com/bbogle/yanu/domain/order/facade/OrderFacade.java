525c3e2591e5f527eab857ee9fcd53fb60c11eaf
seonyo <jsy6535@naver.com>
2024-06-18 08:44:48 +0900

    :bug: FindOrderService isReview 올바른 데이터 넘겨주기

--- src/main/java/com/bbogle/yanu/domain/order/facade/OrderFacade.java
+++ src/main/java/com/bbogle/yanu/domain/order/facade/OrderFacade.java
@@ -2,6 +2,7 @@ package com.bbogle.yanu.domain.order.facade;
 
 import com.bbogle.yanu.domain.order.repository.OrderRepository;
 import com.bbogle.yanu.domain.product.domain.ProductEntity;
+import com.bbogle.yanu.domain.review.domain.ReviewEntity;
 import com.bbogle.yanu.domain.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +11,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OrderFacade {
     private final ReviewRepository reviewRepository;
-    public boolean checkIsReview(ProductEntity product, Long userId){
-        Long productId = product.getId();
-
-        return reviewRepository.existsByUserIdAndProductId(userId, productId);
+    public boolean checkIsReview(ReviewEntity review){
+        if(review == null)
+            return true;
+        else
+            return false;
     }
 }
